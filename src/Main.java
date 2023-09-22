d8228f891179b445103e423a4cc772e76e71b9ca
Pedro Oliveira <pedrooli@pop-os.localdomain>
2023-09-22 11:06:55 -0300

    comments and small changes

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,22 @@ import db.Database;
 public class Main {
 
     public static void main(String[] args) {
-        Database bancoDeDados = new Database();
+        Database database = new Database();
+//      aqui inicializamos nossa classe database;
+//      sua funcao, como diz o nome instancia um banco de dados em uma stack,
+//      de modo que nossos dados sejam armazenados para futuras consultas
 
-        Movie novoFilme =  new Movie("filme sobre qualquer coisa", 2005, "acao");
-        bancoDeDados.newMovie(novoFilme);
+        Movie newMovie =  new Movie("filme sobre qualquer coisa", 2005, "acao");
+        database.newMovie(newMovie);
+//      instanciamos uma nova classe de Movie, seu constructor, pede as informacoes que nos interessam;
+//      temos que salvar nosso novo filme no banco de dados
 
-        System.out.println(bancoDeDados.movieDatabase.get(0));
+        System.out.println(database.movieDatabase.get(0));
+//      aqui podemos pegar o filme que foi instanciado e acessar os parametros da classe.
     }
 }
+//      a fazer:
+//          1 - criar uma funcao que filtre o input do usuario que seria um genero
+//                  e retorne uma Stack dos filmes filtrados;
+//          2 - criar uma funcao que pegue uma Stack<>(lista) de filmes e retorne um filme
+//                  aletorio desta lista.
