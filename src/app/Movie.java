ddfc02d3de8107dbea0af0e183e3a93322548cc1
Cindy Fagundes <144467488+cindyfagundes@users.noreply.github.com>
2023-09-26 22:40:50 -0300

    Update Movie.java

--- src/app/Movie.java
+++ src/app/Movie.java
@@ -7,9 +7,36 @@ public class Movie {
     public String roteiro;
     public int ano;
     public String genero;
+    public String nome;
     public Movie(String roteiro, int ano, String genero){
         this.roteiro = roteiro;
         this.ano = ano;
         this.genero = genero;
+        this.nome = nome;
     }
+    public String getGenero(){
+        returne genero;
+    }
+
+    public static Stack <Movie>
+        filtrarMoviePorGenero(String genero List<Movie>movie){
+            Stack<Movie>filmesFiltrados=new Stack<>();
+
+            for (Movie movie:movies){
+                if (movie.getGenero().equalsIgnoreCase(genero))
+                {
+                    movieFiltrados.push(filme);
+                }
+            }
+
+            return movieFiltrados;
+
+        }
+    
+    public static void main (String[]args){
+        List<Movie> listaMovie = new ArrayList<>();
+        listaMovie.add(new Movie ("Filme 1 "terror"));
+        listaMovie.add(new Movie ("Filme 2 "suspense"));
+        listaMovie.add(new Movie ("Filme 3 "acao"));
+        listaMovie.add(new Movie ("Filme 4 "comedia"));
 }
